274cd68a0e348e17c759ce98efa0f533c1a39d3a
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2023-03-06 19:23:44 +0100

    Added logout button

--- templates/Users/index.php
+++ templates/Users/index.php
@@ -5,7 +5,8 @@
  */
 ?>
 <div class="users index content">
-    <?= $this->Html->link(__('New User'), ['action' => 'add'], ['class' => 'button float-right']) ?>
+    <?= $this->Html->link(__('New User'), ['action' => 'add'], ['class' => 'button']) ?>
+    <?= $this->Html->link(__('Logout'), ['action' => 'logout'], ['class' => 'button float-right']) ?>
     <h3><?= __('Users') ?></h3>
     <div class="table-responsive">
         <table>
