a98d638b08e4af5060f7fb149f82cc155b48e7b1
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2023-03-30 18:15:03 +0200

    Queries correction

--- src/Controller/BirthdaysController.php
+++ src/Controller/BirthdaysController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Cake\ORM\Table;
-use Cake\ORM\Query;
 
 /**
  * Birthdays Controller
@@ -42,31 +41,29 @@ class BirthdaysController extends AppController
 
         // Obtener todos los Birthdays entre dos fechas concretas
         $this->Birthdays->find('all')
-            ->where(['created >=' => new DateTime('01/01/2023')])
-            ->andWhere(['created <=' => new DateTime('31/03/2023')])
+            ->where(['created >=' => '01-01-2023'], ['created <=' => '31-03-2023'])
             ->contain(['BirthdayItems', 'BirthdayGuests']);
 
         // Obtener todos los Birthdays mayores que una fecha dada o menores que una fecha dada
         $this->Birthdays->find('all')
-            ->where(['created >' => new DateTime('01/01/2023')])
-            ->orWhere(['created <' => new DateTime('31/03/2023')])
+            ->where('OR' => ['created >=' => '01-01-2023'], ['created <=' => '31-03-2023'])
             ->contain(['BirthdayItems', 'BirthdayGuests']);
 
         // Obtener todos los Birthdays con un Guest determinado
-        $this->Birthdays->findAllByBirthdayGuest('Ann Smith');
+        $this->Birthdays->findAllByBirthdayGuest('Ann Smith'); // cambiar
 
         // Obtener todos los Birthdays con un Item determinado
-        $this->Birthdays->findAllByBirthdayItem('Lego City');
+        $this->Birthdays->findAllByBirthdayItem('Lego City'); // cambiar
 
         // Obtener todos los Birthdays sin Items
-        $this->Birthdays->findAllByBirthdayItem(null);
+        $this->Birthdays->findAllByBirthdayItem(null); // cambiar
 
         // Obtener todos los BirthdayItems y BirthdayGuests para un Birthday determinado
         $this->Birthdays->findById('2')
             ->contain(['BirthdayItems', 'BirthdayGuests']);
 
         // Obtener el último Birthday creado por un User determinado
-        $this->Birthdays->findAllByUser('John Doe')->find('recent');
+        $this->Birthdays->findAllByUser('John Doe')->find('recent'); // cambiar a ->first() y el orden
     }
 
     /**
--- src/Model/Entity/Birthday.php
+++ src/Model/Entity/Birthday.php
@@ -31,5 +31,7 @@ class Birthday extends Entity
         'user_id' => true,
         'day' => true,
         'user' => true,
+        'birthday_item' => true,
+        'birthday_guest' => true
     ];
 }
