37aed548c2ae168f51e4cbbe9a40a0ca2b4526d3
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2023-04-04 12:40:15 +0200

    Solve login issue

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -11,6 +11,26 @@ namespace App\Controller;
  */
 class UsersController extends AppController
 {
+    public function login()
+    {
+        $result = $this->Authentication->getResult();
+        // If the user is logged in send them away.
+        if ($result->isValid()) {
+            $target = $this->Authentication->getLoginRedirect() ?? '/home';
+            return $this->redirect($target);
+        }
+        if ($this->request->is('post')) {
+            $this->Flash->error('Invalid username or password');
+        }
+    }
+
+    public function beforeFilter(\Cake\Event\EventInterface $event)
+    {
+        parent::beforeFilter($event);
+
+        $this->Authentication->allowUnauthenticated(['login']);
+    }
+
     /**
      * Index method
      *
@@ -101,5 +121,11 @@ class UsersController extends AppController
         }
 
         return $this->redirect(['action' => 'index']);
+    }
+
+    public function logout()
+    {
+        $this->Authentication->logout();
+        return $this->redirect(['controller' => 'Users', 'action' => 'login']);
     }
 }
