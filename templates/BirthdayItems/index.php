6a7383880e9967fd58fbd433d610a0d87f68a492
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2023-04-06 15:33:18 +0200

    Change styles BirthdayGuests/index.php and BirthdayItems/index.php

--- templates/BirthdayItems/index.php
+++ templates/BirthdayItems/index.php
@@ -3,12 +3,13 @@
  * @var \App\View\AppView $this
  * @var iterable<\App\Model\Entity\BirthdayItem> $birthdayItems
  */
+$this->setLayout('default');
 ?>
-<div class="birthdayItems index content">
-    <?= $this->Html->link(__('New Birthday Item'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Birthday Items') ?></h3>
+
+<div class="birthdayItems index content border border-2 border-success shadow">
+    <h3 class="text-center fw-bold fs-1 text-success"><?= __('Birthday Items') ?></h3>
     <div class="table-responsive">
-        <table>
+        <table class="table table-borderless table-hover mt-4">
             <thead>
                 <tr>
                     <th><?= $this->Paginator->sort('id') ?></th>
@@ -17,7 +18,7 @@
                     <th><?= $this->Paginator->sort('birthday_id') ?></th>
                     <th><?= $this->Paginator->sort('created') ?></th>
                     <th><?= $this->Paginator->sort('modified') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
+                    <th class="actions text-body-secondary"><?= __('Actions') ?></th>
                 </tr>
             </thead>
             <tbody>
@@ -39,14 +40,21 @@
             </tbody>
         </table>
     </div>
-    <div class="paginator">
-        <ul class="pagination">
+    <div class="paginator mt-4">
+        <ul class="pagination justify-content-center">
             <?= $this->Paginator->first('<< ' . __('first')) ?>
             <?= $this->Paginator->prev('< ' . __('previous')) ?>
             <?= $this->Paginator->numbers() ?>
             <?= $this->Paginator->next(__('next') . ' >') ?>
             <?= $this->Paginator->last(__('last') . ' >>') ?>
         </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
+        <p class="text-muted"><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
     </div>
+    <?= $this->Html->link(__('New Birthday Item'), ['action' => 'add'], ['class' => 'btn btn-warning btn-lg w-100 mt-4 fw-semibold fs-4 text-body-secondary text-uppercase']) ?>
 </div>
+
+<?= $this->Html->css('/bootstrap/css/bootstrap.min') ?>
+<?= $this->Html->css(['normalize.min', 'milligram.min', 'cake']) ?>
+<?= $this->Html->script('/bootstrap/css/bootstrap.bundle.min') ?>
+<?= $this->fetch('css') ?>
+<?= $this->fetch('script') ?>
