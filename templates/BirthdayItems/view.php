648a43ac33b4e438f827f7d24d1e3d03b2655a03
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2023-04-07 17:10:59 +0200

    Change the rest of view.php

--- templates/BirthdayItems/view.php
+++ templates/BirthdayItems/view.php
@@ -3,46 +3,49 @@
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\BirthdayItem $birthdayItem
  */
+$this->setLayout('default');
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('Edit Birthday Item'), ['action' => 'edit', $birthdayItem->id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete Birthday Item'), ['action' => 'delete', $birthdayItem->id], ['confirm' => __('Are you sure you want to delete # {0}?', $birthdayItem->id), 'class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('List Birthday Items'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('New Birthday Item'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="birthdayItems view content">
-            <h3><?= h($birthdayItem->name) ?></h3>
-            <table>
-                <tr>
-                    <th><?= __('Name') ?></th>
-                    <td><?= h($birthdayItem->name) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Birthday') ?></th>
-                    <td><?= $birthdayItem->has('birthday') ? $this->Html->link($birthdayItem->birthday->title, ['controller' => 'Birthdays', 'action' => 'view', $birthdayItem->birthday->id]) : '' ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($birthdayItem->id) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Units') ?></th>
-                    <td><?= $this->Number->format($birthdayItem->units) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Created') ?></th>
-                    <td><?= h($birthdayItem->created) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Modified') ?></th>
-                    <td><?= h($birthdayItem->modified) ?></td>
-                </tr>
-            </table>
-        </div>
+
+<div class="btn-group w-100 my-3" role="group">
+    <button type="button" class="btn btn-lg btn-success"><?= $this->Html->link(__('Edit Birthday Item'), ['action' => 'edit', $birthdayItem->id], ['class' => 'side-nav-item text-white fw-bold']) ?></button>
+    <button type="button" class="btn btn-lg btn-success"><?= $this->Form->postLink(__('Delete Birthday Item'), ['action' => 'delete', $birthdayItem->id], ['confirm' => __('Are you sure you want to delete # {0}?', $birthdayItem->id), 'class' => 'side-nav-item text-white fw-bold']) ?></button>
+    <button type="button" class="btn btn-lg btn-success"><?= $this->Html->link(__('List Birthday Items'), ['action' => 'index'], ['class' => 'side-nav-item text-white fw-bold']) ?></button>
+    <button type="button" class="btn btn-lg btn-success"><?= $this->Html->link(__('New Birthday Item'), ['action' => 'add'], ['class' => 'side-nav-item text-white fw-bold']) ?></button>
+</div>
+<div class="card text-secondary mb-5 border-success border-2 shadow">
+    <div class="container">
+        <h3 class="fw-bold text-success text-center mt-3"><?= h($birthdayItem->name) ?></h3>
+        <table>
+            <tr>
+                <th><?= __('Name') ?></th>
+                <td><?= h($birthdayItem->name) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Birthday') ?></th>
+                <td><?= $birthdayItem->has('birthday') ? $this->Html->link($birthdayItem->birthday->title, ['controller' => 'Birthdays', 'action' => 'view', $birthdayItem->birthday->id]) : '' ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Id') ?></th>
+                <td><?= $this->Number->format($birthdayItem->id) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Units') ?></th>
+                <td><?= $this->Number->format($birthdayItem->units) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Created') ?></th>
+                <td><?= h($birthdayItem->created) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Modified') ?></th>
+                <td><?= h($birthdayItem->modified) ?></td>
+            </tr>
+        </table>
     </div>
 </div>
+
+<?= $this->Html->css('/bootstrap/css/bootstrap.min') ?>
+<?= $this->Html->css(['normalize.min', 'milligram.min', 'cake']) ?>
+<?= $this->Html->script('/bootstrap/css/bootstrap.bundle.min') ?>
+<?= $this->fetch('css') ?>
+<?= $this->fetch('script') ?>
--- templates/Birthdays/view.php
+++ templates/Birthdays/view.php
@@ -3,38 +3,41 @@
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Birthday $birthday
  */
+$this->setLayout('default');
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('Edit Birthday'), ['action' => 'edit', $birthday->id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete Birthday'), ['action' => 'delete', $birthday->id], ['confirm' => __('Are you sure you want to delete # {0}?', $birthday->id), 'class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('List Birthdays'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('New Birthday'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="birthdays view content">
-            <h3><?= h($birthday->title) ?></h3>
-            <table>
-                <tr>
-                    <th><?= __('Title') ?></th>
-                    <td><?= h($birthday->title) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('User') ?></th>
-                    <td><?= $birthday->has('user') ? $this->Html->link($birthday->user->id, ['controller' => 'Users', 'action' => 'view', $birthday->user->id]) : '' ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($birthday->id) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Day') ?></th>
-                    <td><?= h($birthday->day) ?></td>
-                </tr>
-            </table>
-        </div>
+
+<div class="btn-group w-100 my-3" role="group">
+    <button type="button" class="btn btn-lg btn-success"><?= $this->Html->link(__('Edit Birthday'), ['action' => 'edit', $birthday->id], ['class' => 'side-nav-item text-white fw-bold']) ?></button>
+    <button type="button" class="btn btn-lg btn-success"><?= $this->Form->postLink(__('Delete Birthday'), ['action' => 'delete', $birthday->id], ['confirm' => __('Are you sure you want to delete # {0}?', $birthday->id), 'class' => 'side-nav-item text-white fw-bold']) ?></button>
+    <button type="button" class="btn btn-lg btn-success"><?= $this->Html->link(__('List Birthdays'), ['action' => 'index'], ['class' => 'side-nav-item text-white fw-bold']) ?></button>
+    <button type="button" class="btn btn-lg btn-success"><?= $this->Html->link(__('New Birthday'), ['action' => 'add'], ['class' => 'side-nav-item text-white fw-bold']) ?></button>
+</div>
+<div class="card text-secondary mb-5 border-success border-2 shadow">
+    <div class="container">
+        <h3 class="fw-bold text-success text-center mt-3"><?= h($birthday->title) ?></h3>
+        <table>
+            <tr>
+                <th><?= __('Title') ?></th>
+                <td><?= h($birthday->title) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('User') ?></th>
+                <td><?= $birthday->has('user') ? $this->Html->link($birthday->user->id, ['controller' => 'Users', 'action' => 'view', $birthday->user->id]) : '' ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Id') ?></th>
+                <td><?= $this->Number->format($birthday->id) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Day') ?></th>
+                <td><?= h($birthday->day) ?></td>
+            </tr>
+        </table>
     </div>
 </div>
+
+<?= $this->Html->css('/bootstrap/css/bootstrap.min') ?>
+<?= $this->Html->css(['normalize.min', 'milligram.min', 'cake']) ?>
+<?= $this->Html->script('/bootstrap/css/bootstrap.bundle.min') ?>
+<?= $this->fetch('css') ?>
+<?= $this->fetch('script') ?>
