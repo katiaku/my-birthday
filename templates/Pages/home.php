e5ab7cbe68147d3b7a13103884dfdf09347fe82d
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-06-26 18:11:56 +0200

    Add style corrections

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -54,7 +54,7 @@ endif;
     <?= $this->Html->charset() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>
-        CakePHP: the rapid development PHP framework:
+        Birthday Planner
         <?= $this->fetch('title') ?>
     </title>
     <?= $this->Html->meta('icon') ?>
